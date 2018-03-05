d1d5fc42413e3227f72e0b7f5cf5c093ebeb8eb7
Rodrigo Gonzalez <rodrigo@axented.com>
2018-03-05 11:20:16 -0600

    Add interactivity
    Tappable heart icons to each row.
    When the user taps an entry in the list, toggling its “favorited” state, that word pairing is added or removed from a set of saved favorites.

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestion = <WordPair>[];
+  final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
@@ -34,14 +35,28 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-   Widget _buildRow(WordPair pair) {
-      return new ListTile(
-        title: new Text(
-          pair.asPascalCase,
-          style: _biggerFont,
-        )
-      );
-    }
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState((){
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
 
   Widget _buildSuggestion() {
     return new ListView.builder(
